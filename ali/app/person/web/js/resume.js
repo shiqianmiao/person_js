b534e5a3bead5a85124b88c61332ed929db98c4e
shiqianmiao <1205924828@qq.com>
2014-07-20 17:55:51 +0800

    submit stickup js

---
+++ ali/app/person/web/js/resume.js
@@ -0,0 +1,12 @@
+/**
+ *@desc主业js实现 
+ */
+var ResumeModule = exports;
+var $ = require('jquery');
+
+ResumeModule.init = function() {
+	$('#js-resume-title').fadeIn();
+	$('#js-resume-head').fadeIn(1500);
+	//$('#js-resume-title').slideUp(1000);
+}
+
