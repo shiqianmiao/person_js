1331efabba10b02b2351b8e94679bb71673be4f8
shiqianmiao <1205924828@qq.com>
2014-07-21 20:14:28 +0800

    submit

--- ali/app/person/web/js/resume.js
+++ ali/app/person/web/js/resume.js
@@ -5,8 +5,6 @@ var ResumeModule = exports;
 var $ = require('jquery');
 
 ResumeModule.init = function() {
-	$('#js-resume-title').fadeIn();
 	$('#js-resume-head').fadeIn(1500);
-	//$('#js-resume-title').slideUp(1000);
 }
 
